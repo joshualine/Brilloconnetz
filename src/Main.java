7764c2e38eb9f4369f0453d9c423e5c37d397cae
Joshua Chinwendu <joshuachinwendu@gmail.com>
2023-08-22 19:47:15 +0100

    Final for Non-concurrent approach

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,46 @@
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-import java.time.*;
-import java.time.format.DateTimeParseException;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int totalTime = 0;
+        int pancakesMade;
+        int[] pancakesEaten = new int[3];
+        int[] usersRequests = new int[3];
 
-        System.out.print("Username: ");
-        String username = scanner.nextLine();
+        Random random = new Random();
 
-        System.out.print("Email: ");
-        String email = scanner.nextLine();
+        while (totalTime < 3600) { // Simulate for 1 hour (3600 seconds)
+            int startTime = totalTime;
+            int endTime = totalTime + 30;
 
-        System.out.print("Password: ");
-        String password = scanner.nextLine();
+            pancakesMade = Math.min(random.nextInt(13), 12); // Generate pancakes made by shopkeeper
 
-        System.out.print("Date of Birth (yyyy-MM-dd): ");
-        String dobString = scanner.nextLine();
-
-        List<CompletableFuture<String>> validationTasks = new ArrayList<>();
-        validationTasks.add(CompletableFuture.supplyAsync(() -> validateUsername(username)));
-        validationTasks.add(CompletableFuture.supplyAsync(() -> validateEmail(email)));
-        validationTasks.add(CompletableFuture.supplyAsync(() -> validatePassword(password)));
-        validationTasks.add(CompletableFuture.supplyAsync(() -> validateDOB(dobString)));
-
-        CompletableFuture<Void> allOf = CompletableFuture.allOf(validationTasks.toArray(new CompletableFuture[0]));
-
-        allOf.thenRun(() -> {
-            List<String> validationFailures = new ArrayList<>();
-            for (CompletableFuture<String> task : validationTasks) {
-                String failureMessage = task.join();
-                if (failureMessage != null) {
-                    validationFailures.add(failureMessage);
-                }
+            for (int i = 0; i < 3; i++) {
+                usersRequests[i] = random.nextInt(6); // Generate user requests (0-5 pancakes)
+                usersRequests[i] = Math.min(usersRequests[i], 5); // Cap at 5 pancakes per user
+                pancakesEaten[i] += usersRequests[i]; // Track total eaten by user i
             }
 
-            if (validationFailures.isEmpty()) {
-                System.out.println("All criteria are met. Validation successful!");
-            } else {
-                System.out.println("Validation failures:");
-                for (String failure : validationFailures) {
-                    System.out.println("- " + failure);
-                }
+            int totalPancakesEaten = usersRequests[0] + usersRequests[1] + usersRequests[2];
+            boolean metNeeds = totalPancakesEaten <= pancakesMade;
+
+            int wastedPancakes = metNeeds ? (pancakesMade - totalPancakesEaten) : pancakesMade;
+
+            System.out.println("Time Slot: " + startTime + "s - " + endTime + "s");
+            System.out.println("Pancakes made by shopkeeper: " + pancakesMade);
+            System.out.println("Pancakes eaten by User 1: " + usersRequests[0]);
+            System.out.println("Pancakes eaten by User 2: " + usersRequests[1]);
+            System.out.println("Pancakes eaten by User 3: " + usersRequests[2]);
+            System.out.println("Shopkeeper met users' needs: " + metNeeds);
+            System.out.println("Wasted pancakes: " + wastedPancakes);
+            if (!metNeeds) {
+                int unmetOrders = totalPancakesEaten - pancakesMade;
+                System.out.println("Unmet pancake orders: " + unmetOrders);
             }
-        });
+            System.out.println("------------------------------------");
 
-        // Wait for the validation tasks to complete
-        allOf.join();
-    }
-
-    public static String validateUsername(String username) {
-        if (username.isEmpty() || username.length() < 4) {
-            return "Username must not be empty and must have at least 4 characters.";
+            totalTime += 30; // Move to the next time slot
         }
-        return null;
-    }
-
-    public static String validateEmail(String email) {
-        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
-        Pattern pattern = Pattern.compile(emailRegex);
-        Matcher matcher = pattern.matcher(email);
-        if (!email.isEmpty() && !matcher.matches()) {
-            return "Email must not be empty and must be a valid email address.";
-        }
-        return null;
-    }
-
-    public static String validatePassword(String password) {
-        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#$%^&*])(?=\\S+$).{8,}$";
-        if (!password.isEmpty() && !password.matches(passwordRegex)) {
-            return "Password must not be empty, and must be strong (1 uppercase, 1 special character, 1 number, 8 characters minimum).";
-        }
-        return null;
-    }
-
-    public static String validateDOB(String dobString) {
-        try {
-            LocalDate dob = LocalDate.parse(dobString);
-            LocalDate minAgeDate = LocalDate.now().minusYears(16);
-            if (!dob.isBefore(minAgeDate)) {
-                return "Date of Birth must not be empty and the person should be at least 16 years old.";
-            }
-        } catch (DateTimeParseException e) {
-            return "Date of Birth must be in the format yyyy-MM-dd.";
-        }
-        return null;
     }
 }
